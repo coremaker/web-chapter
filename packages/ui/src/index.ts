1a2aefd7f77ffdb62140ee9e438969cce794ff64
Eduard Valentin <eduard.valentin1996@gmail.com>
2023-03-09 23:45:22 +0200

    renamed exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -4,13 +4,13 @@ export { default as Modal } from "@mui/material/Modal";
 
 export { default as Select } from "@mui/material/Select";
 
-export { ThemeProvider } from "@mui/material";
+export { default as ThemeProvider } from "@mui/material/styles/ThemeProvider";
 
 export { default as Typography } from "@mui/material/Typography";
 
 export { default as theme } from "./components/theme/theme";
 
-export { createTheme } from "@mui/material";
+export { default as createTheme } from "@mui/material/styles/createTheme";
 
 export * from "@mui/material";
 export * from "@mui/material/styles";
