1c4c649235480f0339e08553a4dda6841b45d536
Alex Prapugicu <paxthedev@gmail.com>
2023-03-14 11:26:44 +0200

    export OptionUnstyled from material base

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,14 +1,15 @@
-export { default as ThemeProvider } from "@mui/material/styles/ThemeProvider";
+export { default as ThemeProvider } from '@mui/material/styles/ThemeProvider';
 
-export { default as theme } from "./components/theme/theme";
+export { default as theme } from './components/theme/theme';
 
-export { default as createTheme } from "@mui/material/styles/createTheme";
+export { default as createTheme } from '@mui/material/styles/createTheme';
 
-export { default as SelectUnstyled } from "@mui/base/SelectUnstyled";
-export { default as ButtonUnstyled } from "@mui/base/ButtonUnstyled";
-export { default as InputUnstyled } from "@mui/base/InputUnstyled";
+export { default as SelectUnstyled } from '@mui/base/SelectUnstyled';
+export { default as OptionUnstyled } from '@mui/base/OptionUnstyled';
+export { default as ButtonUnstyled } from '@mui/base/ButtonUnstyled';
+export { default as InputUnstyled } from '@mui/base/InputUnstyled';
 
-export * from "@mui/material";
-export * from "@mui/material/styles";
-export * from "@mui/material/Button";
-export * from "@mui/material/Typography";
+export * from '@mui/material';
+export * from '@mui/material/styles';
+export * from '@mui/material/Button';
+export * from '@mui/material/Typography';
