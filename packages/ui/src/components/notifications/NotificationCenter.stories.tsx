01087358d6139160e8298b563a5c6c0667feca4c
Alex Prapugicu <alex.prapugicu@coremaker.io>
2023-08-25 09:27:26 +0300

    style notifications for storyfile demo

--- packages/ui/src/components/notifications/NotificationCenter.stories.tsx
+++ packages/ui/src/components/notifications/NotificationCenter.stories.tsx
@@ -1,25 +1,32 @@
 import { Meta, StoryFn } from '@storybook/react';
 import NotificationCenter, { notify } from './NotificationCenter';
 import { Button } from '@mui/material';
+import styles from './styles.module.css';
 
 export default {
     title: 'Example/NotificationCenter',
     component: NotificationCenter,
 } as Meta<typeof NotificationCenter>;
 
-const Template: StoryFn<typeof NotificationCenter> = (args) => (
-    <div>
-        <NotificationCenter {...args} />
+type NotificationCenterProps = React.ComponentProps<typeof NotificationCenter>;
 
-        <Button onClick={() => notify({ type: 'error', message: 'Test' })}>Trigger notification</Button>
-    </div>
-);
+const Template: StoryFn<NotificationCenterProps> = (args) => {
+    return (
+        <>
+            <NotificationCenter {...args} />
+            <Button onClick={() => notify({ message: 'test', type: 'info' })}>Notify</Button>
+        </>
+    );
+};
 
-export const Base = Template.bind({});
-Base.args = {
-    hideAfter: 5000,
-    maxNotifications: 10,
-    SnackbarComponent: ({ id, open }) => {
-        return open ? <div>SnackbarComponent {id}</div> : null;
-    },
+export const Default = Template.bind({});
+Default.args = {
+    className: styles.wrapper,
+    SnackbarComponent: (props: any) => (
+        <button className={styles.button} type="button" onClick={props.onClose} key={props.id}>
+            <div>
+                {props.message} {props.id}
+            </div>
+        </button>
+    ),
 };
