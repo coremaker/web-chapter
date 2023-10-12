3843c405ddf22ffcd0c89201eb163d551a26636e
Adrian-Coremaker <adrian@coremaker.io>
2023-10-12 17:46:24 +0300

    remove storybook static

--- packages/ui/storybook-static/components/Spinner/Spinner.d.ts
+++
@@ -1,3 +0,0 @@
-import { CircularProgressProps } from '@mui/material';
-declare const Spinner: (props: CircularProgressProps) => import("react/jsx-runtime").JSX.Element;
-export default Spinner;
--- packages/ui/storybook-static/components/Table/components/SearchEmptyState.d.ts
+++
@@ -1,2 +0,0 @@
-declare const SearchEmptyState: () => import("react/jsx-runtime").JSX.Element;
-export default SearchEmptyState;
